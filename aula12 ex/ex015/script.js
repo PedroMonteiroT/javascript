21342cdac272ecd164ec9b22c7737942d271fa0f
PedroDaAlanis <pmtex06@gmail.com>
2023-08-09 14:28:17 -0300

    ble

--- aula12 ex/ex015/script.js
+++ aula12 ex/ex015/script.js
@@ -3,4 +3,36 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('verifique os dados e tente novamente!')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if(fsex[0].checked){
+            genero='Homem'
+            if(idade>=0 && idade<10){
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade<21){
+                img.setAttribute('src', 'homem.jpg')
+            }else{
+                img.setAttribute('src', 'idoso.jpg')
+            }
+        }else if(fsex[1].checked){
+            genero='Mulher'
+            if(idade>=0 && idade<10){
+                img.setAttribute('src', 'menina.jpg')
+            } else if (idade<21){
+                img.setAttribute('src', 'mulher.jpg')
+            }else{
+                img.setAttribute('src', 'idosa.jpg')
+            }
+        }
+        res.style.textAlign='center'
+        res.innerHTML = 'Detectamos ' + genero + ' de ' + idade + ' anos'
+        res.appendChild(img)
+    }
 }
---
+++ aula13/ambiente.js
@@ -0,0 +1,5 @@
+i=1
+while(i<=f10){
+    console.log(i)
+    i++
+}
